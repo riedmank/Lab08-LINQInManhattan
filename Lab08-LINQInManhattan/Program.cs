680e5f59a8e40e26586741a8a1dc490abdb63a67
Kristofer Riedman <riedmank@hotmail.com>
2018-10-19 10:30:18 -0700

    all questions answered

--- Lab08-LINQInManhattan/Program.cs
+++ Lab08-LINQInManhattan/Program.cs
@@ -26,29 +26,67 @@ namespace Lab08_LINQInManhattan
 
             var data = JsonConvert.DeserializeObject<RootObject>(text);
 
-            var neighborhood = data.Features.Select(x => x).Select(x => x.Properties);
+            var neighborhood = data.Features.Select(x => x).Select(x => x.Properties).Select(x => x.Neighborhood);
 
             Console.WriteLine("====================================================");
             Console.WriteLine("Question1: Output all Neighborhoods");
             Console.WriteLine("====================================================");
 
-            var neighborhoodNames = neighborhood.Select(x => x.Neighborhood);
+            var neighborhoodNames = neighborhood.Select(x => x);
 
             foreach (var item in neighborhoodNames)
             {
                 Console.WriteLine(item);
             }
 
-            neighborhoodNames = neighborhood.Where(x => x.Neighborhood.Length > 0).Select(x => x.Neighborhood);
-            
             Console.WriteLine("====================================================");
             Console.WriteLine("Question2: Filter out neighborhoods without names");
             Console.WriteLine("====================================================");
 
-            foreach (var item in neighborhoodNames)
+            var noEmptyNeighborhoodNames = neighborhoodNames.Where(x => x.Length > 0);
+
+            foreach (var item in noEmptyNeighborhoodNames)
             {
                 Console.WriteLine(item);
             }
+
+            Console.WriteLine("====================================================");
+            Console.WriteLine("Question3: Remove duplicates");
+            Console.WriteLine("====================================================");
+
+            var uniqueNeighborhoodNames = noEmptyNeighborhoodNames.Select(x => x).Distinct();
+
+            foreach (var item in uniqueNeighborhoodNames)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("====================================================");
+            Console.WriteLine("Question4: Consolidate into single query");
+            Console.WriteLine("====================================================");
+
+            var singleQuery = neighborhood.Where(x => x.Length > 0).Distinct();
+
+            foreach (var item in singleQuery)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("====================================================");
+            Console.WriteLine("Question5: Do a LINQ Query");
+            Console.WriteLine("====================================================");
+
+            var LINQQuery = from hood in neighborhood
+                            select hood;
+
+            foreach (var hood in LINQQuery)
+            {
+                Console.WriteLine(hood);
+            }
+
+            //from hood in hoods,
+            //select hood;
+
         }
     }
 }
